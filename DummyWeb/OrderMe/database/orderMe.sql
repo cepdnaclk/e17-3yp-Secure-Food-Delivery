80bb62aa8f6367fe4d3d53cdf100298ab039ef32
Nadeesha Diwakara <nadiw9811@gmail.com>
2021-08-18 15:25:55 +0530

    Create database sql script for OrderMe dummy web

---
+++ DummyWeb/OrderMe/database/orderMe.sql
@@ -0,0 +1,27 @@
+create database orderme;
+use orderme;
+
+create table customer
+	(TelNo varchar(10) primary key,
+    CName varchar(20),
+    CPassword varchar(20));
+    
+create table partner
+	(Email varchar(30) primary key,
+    RestName varchar(50),
+    PPassword varchar(20));
+    
+create table confirmOrder
+	(OrderId varchar(10),
+    foreign key(OrderId) references placeOrder(OrderId),
+    CTelNo varchar(10),
+    foreign key(CTelNo) references customer(TelNo),
+    deviceId varchar(10));
+    
+
+create table  placeOrder
+	(OrderId varchar(10) primary key,
+    CTelNo varchar(10),
+    foreign key(CTelNo) references customer(TelNo),
+    Details varchar(50),
+    SFD varchar(3));
