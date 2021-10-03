871a5c78e06629ffc6cf016043ec769835c25f12
Nadeesha Diwakara <nadiw9811@gmail.com>
2021-10-03 18:22:08 +0530

    Updated with HTTPS

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,16 @@ const config = require('config');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 const orderHandle = require('./routes/orderhandle');
+const orderAccess = require('./routes/orderaccess');
+
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+
+let key = fs.readFileSync(__dirname + '/./certs/selfsigned.key');
+let cert = fs.readFileSync(__dirname + '/./certs/selfsigned.crt');
+let options = { key: key, cert: cert };
+
 const app = express();
 
 if (!config.get('jwtPrivateKey')) {
@@ -15,7 +24,9 @@ app.use(express.json());
 app.use('/api/restaurant', restaurant);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
-app.use('/api/orderHandle', orderHandle);
+app.use('/api/order_handle', orderHandle);
+app.use('/api/order_access', orderAccess);
 
+let server = https.createServer(options, app);
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`server listen on ${port}`));
+server.listen(port, () => console.log(`https server listen on ${port}`));
